2470990e343b0295fc902da84979691255cfcbc7
Roma-Leto <119625441+Roma-Leto@users.noreply.github.com>
2024-07-27 13:43:11 +0300

    create socialnetwork (generalapp) add base.html

--- DatingSN/urls.py
+++ DatingSN/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', )
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='app_users')),
 ]
